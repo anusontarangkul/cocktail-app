e73c35c9e1be46f8b9bcdc0918838b70039ebcd8
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-03-11 15:37:29 -0800

    saved page

--- pages/saved.jsx
+++ pages/saved.jsx
@@ -3,6 +3,7 @@ import Nav from '../components/Nav/Nav';
 import Alert from '../components/Alert/Alert';
 import { CocktailState } from '../CocktailContext';
 import SavedResults from '../components/SavedResults/SavedResults';
+import styles from './saved.module.css';
 
 const Saved = () => {
   const { user, saved, setSaved, setAlert } = CocktailState();
@@ -10,7 +11,7 @@ const Saved = () => {
   return (
     <main>
       <Nav />
-      <h2>Saved Cocktails</h2>
+      <h1 className={styles.title}>{saved.length} Saved Cocktails</h1>
       <SavedResults />
       <Alert />
     </main>
